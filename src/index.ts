454182b0531179c18075fd22f3f238ffbc1fc418
Aleksandr <a13x.student@yandex.ru>
2021-09-23 13:14:42 +0300

    deploy check

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { apiHookRout } from './routes'
+// import { apiHookRout } from './routes'
 // require('dotenv').config()
 
 // const PORT = process.env.PORT ?? 5000
@@ -9,7 +9,7 @@ const PORT = 5000
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(apiHookRout)
+// app.use(apiHookRout)
 
 app.listen(PORT, () => {
   console.log('server started on port ', PORT)
