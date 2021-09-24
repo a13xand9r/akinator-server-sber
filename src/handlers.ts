70574ad72390cf06cda3b86b13582154967661c6
a13xand9r <a13x.student@yandex.ru>
2021-09-24 14:53:41 +0300

    changed language

--- src/handlers.ts
+++ src/handlers.ts
@@ -5,9 +5,13 @@ import { AnswerType } from './types';
 import { getRandomFromArray } from './utils/utils';
 import { region } from 'aki-api';
 
-export const runAppHandler: SaluteHandler = ({ res }) => {
-  res.setPronounceText('Hi, now I will be Jin Akinator. Let\'s play and learn English together.')
-  res.appendBubble('Hi, now I will be Jin Akinator. Let\'s play and learn English together.')
+export const runAppHandler: SaluteHandler = ({ req, res }) => {
+  if (req.request.payload.character.appeal === 'official'){
+    res.setPronounceText('<speak>Здравствуйте! Здесь я буду в роли Джина Акина\'тора. Я могу угадать любого персонажа, которого вы загадали и помогу в изучении английского. Давайте играть и учить английский вместе.</speak>')
+    res.appendBubble('Здравствуйте! Здесь я буду в роли Джина Акина\'тора. Я могу угадать любого персонажа, которого вы загадали и помогу в изучении английского. Давайте играть и учить английский вместе.')
+  }
+  res.setPronounceText('<speak>Привет! Здесь я буду в роли Джина Акина\'тора. Я могу угадать любого персонажа, которого ты загадал и помогу в изучении английского. Давай играть и учить английский вместе.</speak>')
+  res.appendBubble('Привет! Здесь я буду в роли Джина Акина\'тора. Я могу угадать любого персонажа, которого ты загадал и помогу в изучении английского. Давай играть и учить английский вместе.')
 }
 
 export const noMatchHandler: SaluteHandler = ({ res }) => {
