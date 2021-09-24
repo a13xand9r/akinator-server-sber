e68e8e9f996943270dca940ffc263f61f2dd08f7
a13xand9r <a13x.student@yandex.ru>
2021-09-24 15:12:35 +0300

    some ch

--- src/akinator.ts
+++ src/akinator.ts
@@ -4,7 +4,7 @@ import { AnswerType } from './types';
 
 export const runAkinator = async (region: region) => {
   const childMode = false
-  const aki = new Aki({ region, childMode, proxyOptions: 'http://46.229.187.169' });
+  const aki = new Aki({ region, childMode });
   try {
     await aki.start();
   } catch (error) {
